b525b4aaf834f1886a318d4ba0b015a56316f0bf
wille318@naver.com <wille318@naver.com>
2020-05-27 02:38:53 +0900

    0527 before {{}}

--- page/views.py
+++ page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html') 
+
+def result(request):
+    return render(request, 'result.html') 
