3babcf02cba87b68094ad577005bb535289d9deb
wille318@naver.com <wille318@naver.com>
2020-05-31 01:18:17 +0900

    result.html 채워넣다가 포기함

--- page/views.py
+++ page/views.py
@@ -6,3 +6,8 @@ def home(request):
 
 def result(request):
     return render(request, 'result.html') 
+
+    #home.html의 번호 6개 받기 {{number}
+    number = request.GET["number1", "number2","number3","number4","number5","number6"]
+
+    #로또 랜덤으로 6개 만들기 {{random}}
