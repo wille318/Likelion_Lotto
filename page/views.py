7fd2841845080e934364dd6f70fb05e3acb5334a
wille318@naver.com <wille318@naver.com>
2020-06-05 18:36:29 +0900

    The end ^add

--- page/views.py
+++ page/views.py
@@ -5,9 +5,25 @@ def home(request):
     return render(request, 'home.html') 
 
 def result(request):
-    return render(request, 'result.html') 
+    #사용자 입력값 받기
+    ulist = []
+    for a in range(6):
+        num1 = request.GET['number' + str(a+1)]
+        num1 = int(num1)
+        ulist.append(num1)
 
-    #home.html의 번호 6개 받기 {{number}
-    number = request.GET["number1", "number2","number3","number4","number5","number6"]
+    #랜덤으로 6개
+    import random
+    rlist = []
+    for b in range(6):
+        num2 = random.randrange(1,46)
+        rlist.append(num2)
 
-    #로또 랜덤으로 6개 만들기 {{random}}
+    #일치 개수
+    count = 0
+    for a in range(6):
+        for b in range(6):
+            if(ulist[a] == rlist[b]) :
+                count += 1
+    
+    return render(request, 'result.html', {'number_list': ulist, 'random_list':rlist, 'count' : count}) 
